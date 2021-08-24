d7c92a5c4e6f9275888d7258470c40a7c659bc59
OCD0711 <conghaohu0711@outlook.com>
2021-08-24 08:55:49 +0800

    lemonade: Initial device conversion

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/havoc_lemonades.mk
+    $(LOCAL_DIR)/aosp_lemonades.mk
 
 COMMON_LUNCH_CHOICES := \
-    havoc_lemonades-user \
-    havoc_lemonades-userdebug \
-    havoc_lemonades-eng
+    aosp_lemonades-user \
+    aosp_lemonades-userdebug \
+    aosp_lemonades-eng
